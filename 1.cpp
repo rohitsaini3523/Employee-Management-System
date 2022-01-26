dd2f526b61bd3bf69c80d30bcf1f2ee72a8edf79
rs3523 <rohitsaini3523@gmail.com>
2022-01-26 13:57:25 +0530

    Yo

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,8 @@
-/* A company pays its employees on a weekly basis. The employees are of 
-three types: teaching employees, non-teaching employees and housekeeping 
-employees. Consider the appropriate formula to calculate the salary of each type 
-of employee. Use late binding functionality to implement an OO application */
-
-#include<iostream>
-#include<stdlib.h>
-#include<conio.h>
-#include<fstream>
-#include<unistd.h>
+#include <iostream>
+#include<iomanip>
+#include <stdlib.h>
+#include <conio.h>
+#include <unistd.h>
 using namespace std;
 
 class employee
@@ -18,68 +13,141 @@ protected:
 	string email_id;
 	string mobile_no;
 	string emp_id;
+	float weeklySal, da, ta, pf;
 
 public:
 	void accept();
 	void display();
+	virtual float calculate_salary() = 0;
 };
 void employee::accept()
 {
 	cout << "\nEmployee Details\n";
-	cout << "Enter Name:";
+	cout << "Enter Name: ";
 	cin >> name;
 	cin.ignore();
-	cout << "Enter Address:";
+	cout << "Enter Address: ";
 	getline(cin, address);
-	cout << "Enter Email Id:";
+	cout << "Enter Email Id: ";
 	cin >> email_id;
-	cout << "Enter Mobile No:";
+	cout << "Enter Mobile No: ";
 	cin >> mobile_no;
-	cout << "\nEnter Employee Id:";
+	cout << "Enter Employee Id: ";
 	cin >> emp_id;
 }
 
 void employee::display()
 {
-	cout << "\nEmployee Details";
-	cout << "\nName:" << name;
-	cout << "\nEmployee Id:" << emp_id;
-	cout << "\nAddress:" << address;
-	cout << "\nEmail Id:" << email_id;
-	cout << "\nMobile No:" << mobile_no;
+	cout << "\n*******Employee Details********";
+	cout << "\nName\t\t:\t" << name;
+	cout << "\nEmployee Id\t:\t" << emp_id;
+	cout << "\nAddress		:\t" << address;
+	cout << "\nEmail Id\t:\t" << email_id;
+	cout << "\nMobile No\t:\t" << mobile_no;
 }
 
 //class teaching-employees
-//class non-teaching-employees 
+//class non-teaching-employees
 //class housekeeping-employees
 
-void admin()
+class teaching_Employee : public employee
+{
+	float basic_sal;
+
+public:
+	float calculate_salary()
+	{
+		basic_sal = 35000;
+		da = 0.97 * basic_sal;
+		ta = 0.1 * basic_sal;
+		pf = 0.12 * basic_sal;
+		weeklySal = (basic_sal + da + ta + pf);
+		return weeklySal;
+	}
+	void display_gross()
+	{
+		display();
+		cout << "\nEmploy Type\t:\tTeaching";
+		cout << "\nGross Salary(Weekly):\t" << calculate_salary() << endl;
+	}
+};
+
+class non_teaching_Employee : public employee
+{
+	float basic_sal;
+
+public:
+	float calculate_salary()
+	{
+		basic_sal = 20000;
+		da = 0.97 * basic_sal;
+		ta = 0.1 * basic_sal;
+		pf = 0.12 * basic_sal;
+		weeklySal = (basic_sal + da + ta + pf);
+		return weeklySal;
+	}
+	void display_gross()
+	{
+		display();
+		cout << "\nEmploy Type\t:\tNon-Teaching";
+		cout << "\nGross Salary(Weekly):\t" << calculate_salary() << endl;
+	}
+};
+
+class housekeeping_Employee : public employee
+{
+	float basic_sal;
+
+public:
+	float calculate_salary()
+	{
+		basic_sal = 15000;
+		da = 0.97 * basic_sal;
+		ta = 0.1 * basic_sal;
+		pf = 0.12 * basic_sal;
+		weeklySal = (basic_sal + da + ta + pf);
+		return weeklySal;
+	}
+	void display_gross()
+	{
+		display();
+		cout << "\nEmployee Type\t:\tHouse-Keeping";
+		cout << "\nGross Salary(Weekly):\t" << calculate_salary() << endl;
+	}
+};
+
+int admin()
 {
 	string userid = "Admin";
 	char password[10] = "1234";
 	string inputuserid, inputpassword;
 	system("cls");
-	cout << "\n\n\n\n\t\tEmployee Management System\n"
-		 << "Enter User Id: ";
+	cout << "\n\n\n\n\t\t**************Employee Management System***************\n";
+	cout<< "**\t\tEnter User Id: ";
 	cin >> inputuserid;
-	cout << "\nEnter Password: ";
+	cout << "**\t\tEnter Password: ";
 	cin >> inputpassword;
 	int n = 3;
-	cout << "Connecting ..";
-	while(n--)
+	char ch;
+	cout << "\t\tConnecting ..";
+	while (n--)
 	{
 		cout << "..";
 	}
 	cout << endl;
 	if (userid == inputuserid)
 	{
-		if(password == inputpassword)
-			cout << "Login Successfully!";
+		if (password == inputpassword)
+		{
+			cout << "\n\t\tLogin Successfully!";
+			sleep(2);
+			return 0;
+		}
 		else if (password != inputpassword)
 		{
-			cout << "Wrong Password\n";
+			cout << "\n\t\tWrong Password\n";
 			sleep(2);
-			admin();
+			return 1;
 		}
 		sleep(1);
 	}
@@ -87,21 +155,78 @@ void admin()
 	{
 		cout << "\n\n\t\tWrong User Id or Password!!!!";
 		sleep(2);
-		admin();
+		return 1;
 	}
 }
+void employe()
+{
+	cout << "Employe!";
+	sleep(2);
+}
+void ADMINCONTROLS()
+{
 
+}
+void LOGIN()
+{
+	int ch;
+	do
+	{
+		system("cls");
+		cout << "\n\n\n\n\t\t**************LOGIN***************\n";
+		cout << "\t\t**\t"<<setw(8)<<"1.Employ\t\t**\n\t\t**\t"<<setw(8)<<"2.Admin\t\t\t**\n\t\t**\t"<<setw(8)<<"3.Exit\t\t\t**\n\t\t**\t"<<setw(8)<<"INPUT CHOICE: ";
+		cin >> ch;
+		cout << "\n\t\t**********************************\n";
+		int n = 3;
+		cout << "\t\tConnecting ..";
+		while (n--)
+		{
+			cout << "..";
+		}
+		sleep(2);
+		if(ch==1)
+		{
+			system("cls");
+			employe();
+		}
+		else if(ch==2)
+		{
+			system("cls");
+			if(admin()==0)
+			{
+				ADMINCONTROLS();
+			}
+			else if (admin()==1)
+			{
+				continue;
+			}
+		}	
+		else if(ch==3)
+		{
+			cout << "\n\t\t******Exiting*******\n";
+			exit(0);
+		}
+		else
+		{
+			cout << "\n\t\t**********INVALID CHOICE*********" ;
+			sleep(2);
+		}
+	} while (ch != 3);
+}
 int main()
 {
-	admin();
-	
-	employee E;
-	E.accept();
-	E.display();
-	fstream f;
-	f.open("Data.txt",ios::out|ios::app);
-	f.write((char *)&f, sizeof(f));
-	f.close();
+	LOGIN();
+
+	/* housekeeping_Employee h;
+	non_teaching_Employee nt;
+	teaching_Employee t;
+	h.accept();
+	nt.accept();
+	t.accept();
+	h.display_gross();
+	nt.display_gross();
+	t.display_gross(); */
+
 	getch();
 	return 0;
 }
